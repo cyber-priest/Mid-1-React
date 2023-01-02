6b0321f2e83061c3617f421cb6a555a83c3563ce
cyber-priest <mikael.dev.official@gmail.com>
2023-01-02 16:44:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [studentData, setStudentData] = useState([])
+  useEffect(() => {
+  axios.get(" http://127.0.0.1:8000/").then((respons)=>setStudentData(respons.data)).catch((error)=>console.log(error))
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {studentData.map((student)=>{
+        return(
+          <div>
+            <h5>{student.name}</h5>
+            <h5>{student.grade}</h5>
+          </div>
+        )
+      })}
     </div>
   );
 }
